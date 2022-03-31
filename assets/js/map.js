827086401d1dcb6c015e14b3e78cc2468ac96403
Tom Ainsworth <tomainsworth94@gmail.com>
2022-03-31 14:36:58 +0100

    Add google map to interest.html

---
+++ assets/js/map.js
@@ -0,0 +1,36 @@
+function initMap() {
+
+    const london = {
+        lat: 51.419289605634674,
+        lng: -0.15940634445235746
+    };
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 1,
+        center: london,
+    })
+    const marker = new google.maps.Marker({
+        position: london,
+        map: map,
+    })
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const locations = [{
+            lat: 40.785091,
+            lng: -73.968285
+        },
+        {
+            lat: 41.084045,
+            lng: -73.874245
+        },
+        {
+            lat: 40.754932,
+            lng: -73.984016
+        }
+    ];
+
+    const markers = locations.map((location, i) => {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length],
+        });
+    });
+}
